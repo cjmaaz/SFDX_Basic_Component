11d27e2c5b030cc8955193459b3d0c417aba1ede
Maaz Rahman <maaz@techie.com>
2021-11-22 16:50:43 +0530

    Create 3. Custom Table with Promise.js

---
+++ 3. Custom Table with Promise.js
@@ -0,0 +1,51 @@
+({
+    doInit : function(component, event, helper) {
+        var dataPromise = helper.fetchLineItems(component, event, helper);
+        dataPromise.then($A.getCallback(function(results){
+            var i = 0;
+            results.forEach((eachLI)=>{
+                if(eachLI.Quantity_Sent__c==undefined || eachLI.Quantity_Sent__c==''){
+                	eachLI.Quantity_Sent__c = 0;
+            	}
+                eachLI.isChecked = false;
+                i++;
+            });
+            component.set('v.totalRecordsCount', i);
+            console.log(JSON.stringify(results));
+            component.set('v.ListPTLI',results);
+            component.set('v.Spinner',false);
+    	})).catch(function(error){
+            component.set('v.Spinner',false);
+      });
+    },
+    checkboxSelect: function(component, event, helper) { 
+        var selectedRec = event.getSource().get("v.value");
+        var getSelectedNumber = component.get("v.selectedCount");
+        if (selectedRec == true) {
+            getSelectedNumber++;
+        } else {
+            getSelectedNumber--;
+            component.find("selectAllId").set("v.value", false);
+        }
+        component.set("v.selectedCount", getSelectedNumber);
+        if (getSelectedNumber == component.get("v.totalRecordsCount")) {
+            component.find("selectAllId").set("v.value", true);
+        }
+    },
+    selectAllCheckbox: function(component, event, helper) {
+        var selectedHeaderCheck = event.getSource().get("v.value");
+        var updatedAllRecords = [];
+        var ListPTLI = component.get("v.ListPTLI");
+        for (var i = 0; i < ListPTLI.length; i++){
+            if (selectedHeaderCheck == true) {
+                ListPTLI[i].isChecked = true;
+                component.set("v.selectedCount", ListPTLI.length);
+            } else {
+                ListPTLI[i].isChecked = false;
+                component.set("v.selectedCount", 0);
+            }
+            updatedAllRecords.push(ListPTLI[i]);
+        }
+        component.set("v.ListPTLI", updatedAllRecords);
+    }
+})
